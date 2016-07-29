aab988d0105b95c477df722cb311cb9320048074
Rory Chatterton <rorychatt@gmail.com>
2016-07-29 21:25:34 +1000

    Reference updates, New share + Fixed Process Crashes

--- SourceFiles/CONTROL/post-install.sh
+++ SourceFiles/CONTROL/post-install.sh
@@ -3,9 +3,8 @@
 case "$APKG_PKG_STATUS" in
         install)
 		ln -s "$APKG_REPO_DIR/mongodb/bin/mongod" "$APKG_PKG_DIR/bin/mongod"
-		mkdir /share/MongoDB/unifi
-		mkdir /share/MongoDB/unifi/db
-		ln -s /share/MongoDB/unifi/db $APKG_PKG_DIR/data/db
+		mkdir /share/Unifi/data
+		ln -s /share/Unifi/data $APKG_PKG_DIR/data
                 ;;
         upgrade)
 		ln -s "$APKG_REPO_DIR/mongodb/bin/mongod" "$APKG_PKG_DIR/bin/mongod"
